c9dab8b46ebd8bc7afa7723490c9e13df67fe090
Doug S <doug.sillars@gmail.com>
2020-03-07 13:46:17 +0100

    playing

--- web-nfc/index.js
+++ web-nfc/index.js
@@ -38,7 +38,7 @@ scanButton.addEventListener("click", async () => {
      	   		case "text":
        				// TODO: Read text record with record data, lang, and encoding.
 					
-					const decoder = new TextDecoder("UTF-16");
+					const decoder = new TextDecoder(record.encoding);
 					 var text = decoder.decode(record.data);
 					log("text payload: " + text );
 					console.log("text payload: " + text );
